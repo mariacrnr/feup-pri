5d4fa6190ebf1c8e1b67584d38662cf1bead5a6d
Maria Carneiro <mariajcarneiro56@gmail.com>
2022-10-02 10:37:16 +0100

    Organized

--- test.py
+++ test.py
@@ -4,26 +4,43 @@ import re
 from sympy import true
 
 
-hitsPerPage=100
-i=0
-hits=1
-while(hits>0):
-    r=requests.get("https://arquivo.pt/textsearch?q=&siteSearch=www.pcp.pt&from=2020&dedupValue="+str(hitsPerPage)+"&maxItems="+str(hitsPerPage)+"&offset="+str(i*hitsPerPage))
-    print("ok")
-    byte_values=r.content
-    data=json.loads(byte_values)
-    hits=len(data["response_items"])
+def text_cleanup(content):
 
-    jsonObjectToStore=[]
-    f = open(str(i)+".json", "w")
-    for item in data["response_items"]:
-        r=requests.get(item["linkToExtractedText"])
-        print(r.status_code)
-        text=r.content.decode("UTF-8",'replace')
-        clean_text=re.sub("[\n\t\r]"," ",text)
-        evenly_spaced_text=re.sub(" +"," ",clean_text)
-        jsonObjectToStore.append({"date":item["tstamp"],"link":item["linkToArchive"],"contentLength":item["contentLength"],"text":evenly_spaced_text})
+    text = content.decode("UTF-8",'replace')
+    clean_text = re.sub("[\n\t\r]"," ",text)
+    evenly_spaced_text = re.sub(" +"," ",clean_text)
+
+    return evenly_spaced_text
+
+def start_data_requests(hits_per_page, date, website):
+
+    hits=1
+    i=0
+
+    while(hits > 0):
+
+        request = requests.get("https://arquivo.pt/textsearch?q=&siteSearch=" + website + "&from="+ str(date) +"&dedupValue=" + str(hits_per_page) + "&maxItems=" +
+            str(hits_per_page)+"&offset="+ str(i * hits_per_page))    
+        print("Request Processed")
+
+        data = json.loads(request.content)
+        hits = len(data["response_items"])
+
+        jsonData = []
+        f = open(str(i)+".json", "w")
+
+        for item in data["response_items"]:
+            next_request = requests.get(item["linkToExtractedText"])
+            print(next_request.status_code)
+
+            text = text_cleanup(next_request.content);
+
+            jsonData.append({"date": item["tstamp"], "link": item["linkToArchive"], "contentLength": item["contentLength"], "text": text})
+            
+        f.write(json.dumps(jsonData, indent=4, ensure_ascii=False))
+        f.close()
+
+        i+=1
         
-    f.write(json.dumps(jsonObjectToStore, indent=4,ensure_ascii=False))
-    f.close()    
-    i+=1
+
+start_data_requests(100, 2020, "www.pcp.pt")
