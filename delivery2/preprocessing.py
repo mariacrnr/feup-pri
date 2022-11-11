1a932f550b15bb50354c444acfc6f52cb7a5dd3e
Maria Carneiro <mariajcarneiro56@gmail.com>
2022-11-11 09:38:49 +0000

    title and party fields and updated dockerfile with correct filenames

---
+++ delivery2/preprocessing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def import_data(group):
+    df = pd.read_json("data/" + group + '.json')
+    return df
+
+def add_title_field(data):
+    # removes the last backlash from the links that have it
+    data['title'] = data['link'].apply(lambda x: x[:-1] if x[-1] == '/' else x)
+
+    # gets only the text after the last backlash
+    data['title'] = data['title'].str.rsplit(pat='/', n=1).str[-1]
+
+    # replaces '-' dividors with empty space
+    data['title'] = data['title'].str.replace('-', ' ')
+
+    # removes file extensions from title
+    data['title'] = data['title'].str.rsplit(pat='.', n=1).str[0]
+
+def add_party_field(party, data):
+    data['party'] = party.upper()
+
+def save_data(group, df):
+    df = df.to_json(r'data/'+ group + '.json', orient='records')
+
+def run():
+    parties = ['ps', 'psd', 'chega', 'pcp', 'bloco', 'il', 'pan', 'livre']
+
+    for party in parties:
+        data = import_data(party)
+        add_title_field(data)
+        add_party_field(party, data)
+        save_data(party, data)
+
+run()
--- delivery2/startup.sh
+++ delivery2/startup.sh
@@ -11,13 +11,21 @@ curl -X POST -H 'Content-type:application/json' \
     http://localhost:8983/solr/parties/schema
 
 # Populate collection
-bin/post -c parties /data/ps_merged_refined.json
+bin/post -c parties /data/ps.json
 
-bin/post -c parties /data/il_refined.json
+bin/post -c parties /data/il.json
 
-bin/post -c parties /data/ch_refined.json
+bin/post -c parties /data/chega.json
 
-bin/post -c parties /data/psd_refined.json
+bin/post -c parties /data/psd.json
+
+bin/post -c parties /data/pcp.json
+
+bin/post -c parties /data/pan.json
+
+bin/post -c parties /data/livre.json
+
+bin/post -c parties /data/bloco.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
