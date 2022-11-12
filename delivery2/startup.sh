7fd593f1117ded15c64a7df7e299970b067b5e69
Miguel Lopes <lopesazevedom@gmail.com>
2022-11-12 15:59:21 +0000

    fix script

--- delivery2/startup.sh
+++ delivery2/startup.sh
@@ -11,21 +11,24 @@ curl -X POST -H 'Content-type:application/json' \
     http://localhost:8983/solr/parties/schema
 
 # Populate collection
-bin/post -c parties /data/ps.json
+# bin/post -c parties /data/ps.json
 
-bin/post -c parties /data/il.json
+# bin/post -c parties /data/il.json
 
-bin/post -c parties /data/chega.json
+# bin/post -c parties /data/chega.json
 
-bin/post -c parties /data/psd.json
+# bin/post -c parties /data/psd.json
 
-bin/post -c parties /data/pcp.json
+# bin/post -c parties /data/pcp.json
 
-bin/post -c parties /data/pan.json
+# bin/post -c parties /data/pan.json
 
-bin/post -c parties /data/livre.json
+# bin/post -c parties /data/livre.json
 
-bin/post -c parties /data/bloco.json
+# bin/post -c parties /data/bloco.json
+
+bin/post -c parties /data/sample.json
+
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
