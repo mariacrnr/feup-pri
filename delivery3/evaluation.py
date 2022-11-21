fd569acdd150830d577c36464fb9a7011a8a7858
Maria Carneiro <mariajcarneiro56@gmail.com>
2022-11-21 16:56:46 +0000

    delivery3

---
+++ delivery3/evaluation.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import requests 
+from sklearn.metrics import PrecisionRecallDisplay
+import matplotlib.pyplot as plt
+import json
+
+def evaluate(system,query_type,indexed):
+
+    queries = pd.read_json('data/queries.json')
+    results=[]
+    mean_avg_precision=[]
+    graphs_data=[]
+    for i in range(5):
+        url = queries.iloc[i][query_type]
+        relevant_documents=queries.iloc[i]['links']
+        response=requests.get(url).json()['response']
+        retrieved_documents=response['numFound']
+        if(retrieved_documents>0):
+            response_df = pd.json_normalize(response['docs'])
+            relevant_documents_retrieved=0
+            relevant_documents_retrieved_10=0
+            
+            positions=list(range(retrieved_documents))
+            relevant=[0 for _ in range(retrieved_documents)]
+
+            for relevant_document in relevant_documents:
+                if(response_df['link'].str.contains(relevant_document,regex=False).any()):
+                    relevant_documents_retrieved+=1
+                if(response_df['link'][0:10].str.contains(relevant_document,regex=False).any()):
+                    relevant_documents_retrieved_10+=1
+                
+                
+            for k in range(retrieved_documents):
+                if(indexed):
+                    link=response_df['link'][k]
+                else:
+                    link=response_df['link'][k][0]
+                if(link in relevant_documents):
+                    if(k==0):
+                        positions[k]=1
+                    else:
+                        positions[k]=positions[k-1]+1
+                    relevant[k]=1
+                else:
+                    if(k!=0):
+                        positions[k]=positions[k-1]
+
+
+            recall_list=list(map(lambda x:x/len(relevant_documents),positions))
+            precision_list=list(range(retrieved_documents))
+            
+            for k in range(retrieved_documents):
+                precision_list[k]=positions[k]/(k+1)
+                
+            avg_precision=relevant[0:10].count(1) if relevant[0:10].count(1)==0 else sum(map(lambda x,y:x*y,precision_list[0:10],relevant[0:10]))/relevant[0:10].count(1)
+
+            
+            graphs_data.append([recall_list[0:10],precision_list[0:10],"Query"+str(i+1)])
+            
+            # plt.show()
+            
+            precision_10=relevant_documents_retrieved_10/10
+            precision=relevant_document if relevant_document==0 else relevant_documents_retrieved/retrieved_documents
+            recall=relevant_documents_retrieved/len(relevant_documents)
+            f_measure=(precision+recall) if (precision+recall)==0 else 2*precision*recall/(precision+recall)
+            
+            print("Query "+str(i+1) +" :")
+            print("\tPrecision: "+str(precision))
+            print("\tP@10: "+str(precision_10))
+            print("\tAvg Precision: "+str(avg_precision))
+
+            print("\tF Measure: "+str(f_measure))
+            print("\tRecall: "+str(recall))
+        else:
+            precision_10=0
+            precision=0
+            recall=0
+            f_measure=0
+            avg_precision=0
+        results.append({'precision':precision,'p10':precision_10,'avg_precision':avg_precision,'f_measure':f_measure,'recall':recall})
+        mean_avg_precision.append(avg_precision)
+    
+    
+    for g in graphs_data:
+        plt.plot(g[0],g[1],marker='o',label=g[2])
+
+    plt.xlim(0, 1.1)
+    plt.ylim(0, 1.1)
+    plt.legend(loc='lower left')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.savefig('evaluation_output/'+system+'/'+query_type+'.pdf')
+    plt.clf()
+    
+    mean_avg_precision=sum(mean_avg_precision)/5
+    results.append({'mean_avg_precision':mean_avg_precision})
+    with open('evaluation_output/'+system+'/'+query_type+'_'+str(i+1)+'.json', 'w') as fp:
+        json.dump(results, fp)
+    
+    
+def run():
+    print("Simple Query:")
+    evaluate('system_1','simple_query',False)
+    print("Complex Query:")
+    evaluate('system_1','complex_query',False)
+    
+run()
---
+++ delivery3/preprocessing.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import datetime
+import json
+import numpy as np
+
+def import_data(group):
+    df = pd.read_json("data/" + group + '.json')
+    df['date']=df['date'].astype(np.int64) // 10**9
+    change_date_field(df)
+    return df
+
+def add_title_field(data):
+    # removes the last backlash from the links that have it
+    data['title'] = data['link'].apply(lambda x: x[:-1] if x[-1] == '/' else x)
+
+    # gets only the text after the last backlash
+    data['title'] = data['title'].str.rsplit(pat='/', n=1).str[-1]
+
+    # replaces '-' dividors with empty space
+    data['title'] = data['title'].str.replace('-', ' ')
+    
+def change_date_field(data):
+    # removes the last backlash from the links that have it
+    data['date'] = data['date'].apply(lambda x: datetime.datetime.fromtimestamp(x).isoformat())
+
+
+
+def add_party_field(party, data):
+    data['party'] = party.upper()
+
+def save_data(group, df):
+    df = df.to_json(r'data/'+ group + '.json', orient='records')
+    
+def create_sample():
+    parties = ['bloco','chega','il','livre','pan','pcp','ps','psd']
+    extras= pd.read_json('data/sample_extras.json', orient='records')
+    
+    final_data=pd.DataFrame()
+    for party in parties:
+  
+        data = import_data(party)
+        final_data = pd.concat([final_data, data.sample(n=200,random_state=0)], ignore_index=True)
+        if(extras['party'].str.contains(party,regex=False).any()):
+            links = extras.loc[extras['party'] == party]
+            links=links.iloc[0]['links']
+            for link in links:
+                if(final_data['link'].str.contains(link,regex=False).any()):
+                    continue
+                final_data = pd.concat([final_data, data.loc[data['link']==link]], ignore_index=True)
+        del data
+    save_data('sample', final_data)
+                 
+
+        
+
+def run():
+    parties = ['bloco','chega','il','livre','pan','pcp','ps','psd']
+
+    for party in parties:
+        print(party)
+        data = import_data(party)
+        add_title_field(data)
+        add_party_field(party, data)
+        save_data(party, data)
+        del data 
+               
+               
+create_sample()
